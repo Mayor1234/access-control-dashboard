c6dd9e5318b37b973b03b709aa9c541bf291f07d
Victor Thompson <thompsonvictor1206@gmail.com>
2025-11-19 22:42:11 +0100

    build production

--- index.ts
+++ index.ts
@@ -3,15 +3,15 @@ import path from 'path';
 import { fileURLToPath } from 'url';
 
 const __filename = fileURLToPath(import.meta.url);
-
 const __dirname = path.dirname(__filename);
 
 const app = express();
 
 app.use(express.static(path.join(__dirname, 'dist')));
 
-app.get('/*', function (req, res) {
+// fallback route, catch all
+app.use((req, res) => {
   res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
 
-app.listen(9001, () => console.log('Server running'));
+app.listen(3001, () => console.log('Server running'));
